79e8f53f8930bcf12d3bd839e6f938365368d308
Leonardo Pepe de Freitas <leopepe@me.com>
2016-09-15 23:28:13 -0300

    Planner incorporating graph characteristics

--- GOAP/Planner.py
+++ GOAP/Planner.py
@@ -1,7 +1,5 @@
-
-
 from collections import deque
-import heapq
+import networkx as nx
 
 
 class Plan:
@@ -25,151 +23,52 @@ class Plan:
         return self.__str__()
 
 
-class PriorityQueue:
-    def __init__(self):
-        self.elements = []
-
-    def empty(self):
-        return len(self.elements) == 0
-
-    def put(self, item, priority):
-        heapq.heappush(self.elements, (priority, item))
-
-    def get(self):
-        return heapq.heappop(self.elements)[1]
-
-
 class Planner:
 
-    def __init__(self, actions: list, states: list, goal: list):
+    def __init__(self, actions, initial_state):
+        self.graph = nx.Graph()
         self.actions = actions
-        self.world = states
-        self.plan = Plan()
-        self.goal = goal
-        # self.breadth_first_search()
+        self.DEBUG = False
+        self.set_nodes()
+        self.set_edges()
 
-    def get_facts(self):
-        return self.world
+    def set_nodes(self):
+        i = 0
+        for action in self.actions:
+            self.graph.add_node(i, attr_dict=action.pre_conditions)
+            if self.DEBUG:
+                print('AUTO Conditions: {0}, i: {1}'.format(action.pre_conditions, i))
+            i += 1
 
-    def get_actions(self):
-        return self.actions
+            if i == len(self.actions):
+                self.graph.add_node(i, attr_dict=action.effects)
 
-    def satisfy_goal(self):
-        g = set(self.goal)
-        f = set(self.world.facts)
-        if g.issubset(f):
-            return True
-        else:
-            return False
+    def set_edges(self):
+        # Test
+        for action in self.actions:
+            # print(action)
+            src = None
+            dst = None
+            obj = None
+            for node in self.graph.nodes(data=True):
+                if action.pre_conditions == node[1]:
+                    src = node[0]
+                    if self.DEBUG:
+                        print('SRC node data {0} = pre_conditions {2}'.format(node, node[1], action.pre_conditions))
 
-    # NOT WORKING PROPERLY
-    def breadth_first_search(self):
-        # plan = self.plan
-        while len(self.actions) != 0:
+                if action.effects == node[1]:
+                    dst = node[0]
+                    obj = action
+                    if self.DEBUG:
+                        print('DST node data {0} = effects {2}'.format(node, node[1], action.effects))
 
-            if self.satisfy_goal():
-                break
+                if src is not None and dst is not None:
+                    self.graph.add_edge(src, dst, object=obj)
+                    if self.DEBUG:
+                        print('Edge created!')
 
-            for action in actions:
-                if action.satisfy_preconditions(self.world.get_facts()):
-                    # self.world.add_fact(action.effects)
-                    [self.world.add_fact(eff) for eff in action.effects]
-                    # [self.world.remove_fact(neff) for neff in action.neg_effects]
-                    self.plan.put(self.actions.get())
-
-        return self.plan
-
-    def a_star_search(self, start, goal):
-
-        def __heuristics(preconds, facts):
-            if preconds <= facts:
-                return True
-
-        visited = []
-        cost = 0
-        while len(actions) != 0:
-            current = self.actions.get()
-
-            if self.satisfy_goal():
-                break
-
-            if current <= visited:
-                continue
-
-            for facts in self.world.facts:
-                if __heuristics(current.pre_conds, facts):
-                    self.plan.put(current)
-                    visited.append(current)
-
-            cost += 1
+    def search(self, ):
 
 
 if __name__ == '__main__':
-    from World import Action
-    from World import World
-
-    facts = ['no_vpc', 'no_vpn', 'no_nat', 'no_asg']
-    world = World(facts=facts)
-    goal = ['asg_exist']
-    actions = Actions()
-    actions.add_action(action=Action(
-        name='create_vpc',
-        pre_conditions=['no_vpc'],
-        post_effects=['vpc_exist'],
-        negative_effects=['no_vpc']
-    ))
-    actions.add_action(
-        Action(
-            name='create_vpn',
-            pre_conditions=['vpc_exist'],
-            post_effects=['vpn_exists'],
-            negative_effects=['no_vpn']
-        )
-    )
-    actions.add_action(
-        Action(
-            name='create_nat',
-            pre_conditions=['vpc_exist'],
-            post_effects=['nat_exist'],
-            negative_effects=['no_nat']
-        )
-    )
-    actions.add_action(
-        Action(
-            name='create_asg',
-            pre_conditions=['vpn_exist'],
-            post_effects=['asg_exist'],
-            negative_effects=['no_asg']
-        )
-    )
-    actions.add_action(
-        Action(
-            name='create_instance',
-            pre_conditions=['vpc_exist', 'vpn_exist'],
-            post_effects=['instance_exist'],
-            negative_effects=['raise_costs']
-        )
-    )
-    actions.add_action(
-        Action(
-            name='destroy_vpn',
-            pre_conditions=['no_nat', 'no_asg'],
-            post_effects=['no_vpn'],
-            negative_effects=['no_vpn']
-        )
-    )
-
-    print('TestCase 1')
-    planner = Planner(actions=actions, world=world, goal=goal)
-    plan = planner.breadth_first_search()
-    print(planner.plan)
-    print(world.facts)
-    print(planner.world.facts)
-    """
-
-    print('TestCase 2')
-    plan = Planner(actions=actions, world=world, goal=goal).astar()
-    print(planner.plan)
-    print(world.facts)
-    print(planner.world.facts)
-    """
+    pass
