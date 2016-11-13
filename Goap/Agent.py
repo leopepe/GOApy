1bead63ab23905cd8f87712095a7963a922d619a
Leonardo Pepe de Freitas <leopepe@me.com>
2016-11-13 18:11:35 -0200

    Agent generates the action plan, executes the action plan and returns the result of the performed actions. README.md with usage example from Agent.py class perspective.

--- Goap/Agent.py
+++ Goap/Agent.py
@@ -1,6 +1,7 @@
 from Goap.Planner import Planner
 """
   DONE
+    - Plan actually works now!
     - Impl. Action
     - Impl. Planner
   TODO
@@ -11,37 +12,65 @@ from Goap.Planner import Planner
 
 class Agent:
     """
-        from Goap.Action import Actions
+    from Goap.Action import Actions
+    import pprint
 
-        import pprint
-        actions = Actions()
-        actions.add_action(
-            name='CreateVPC',
-            pre_conditions={'vpc': False, 'app': False, 'db': False},
-            effects={'vpc': True, 'app': False, 'db': False}
-        )
-        actions.add_action(
-            name='CreateDB',
-            pre_conditions={'vpc': True, 'app': False, 'db': False},
-            effects={'vpc': True, 'app': False, 'db': True}
-        )
-        actions.add_action(
-            name='CreateApp',
-            pre_conditions={'vpc': True, 'app': False, 'db': True},
-            effects={'vpc': True, 'app': True, 'db': True}
-        )
-        init_state = {'vpc': False, 'app': False, 'db': False}
-        init_goal = {'vpc': True, 'db': True, 'app': True}
-        ai_cloud_builder = Agent(name='CloudBuilder', state=init_state, goal=init_goal, actions=actions)
-        result = ai_cloud_builder.actuate()
-        pprint.pprint(result, indent=2, width=80)
+    # ACTIONS
+    actions = Actions()
+    # VPC/Network set
+    actions.add_action(
+        name='CreateVPC',
+        pre_conditions={'vpc': False, 'db': False, 'app': False},
+        effects={'vpc': True, 'db': False, 'app': False}
+    )
+    # DB set
+    actions.add_action(
+        name='CreateDB',
+        pre_conditions={'vpc': True, 'db': False, 'app': False},
+        effects={'vpc': True, 'db': True, 'app': False}
+    )
+    actions.add_action(
+        name='StopDB',
+        pre_conditions={'vpc': True, 'db': 'started', 'app': False},
+        effects={'vpc': True, 'db': 'stopped', 'app': False}
+    )
+    actions.add_action(
+        name='StartDB',
+        pre_conditions={'vpc': True, 'db': 'stopped', 'app': False},
+        effects={'vpc': True, 'db': 'started', 'app': False}
+    )
+    actions.add_action(
+        name='DestroyDB',
+        pre_conditions={'vpc': True, 'db': 'not_health', 'app': False},
+        effects={'vpc': True, 'db': False, 'app': False}
+    )
+    # APP set
+    actions.add_action(
+        name='CreateApp',
+        pre_conditions={'vpc': True, 'db': True, 'app': False},
+        effects={'vpc': True, 'db': True, 'app': True}
+    )
+    actions.add_action(
+        name='StartApp',
+        pre_conditions={'vpc': True, 'db': True, 'app': 'stopped'},
+        effects={'vpc': True, 'db': True, 'app': 'started'}
+    )
+    actions.add_action(
+        name='StopApp',
+        pre_conditions={'vpc': True, 'db': True, 'app': 'started'},
+        effects={'vpc': True, 'db': True, 'app': 'stopped'}
+    )
+    actions.add_action(
+        name='DestroyApp',
+        pre_conditions={'vpc': True, 'db': True, 'app': 'not_health'},
+        effects={'vpc': True, 'db': True, 'app': False}
+    )
+    init_state = {'vpc': False, 'app': False, 'db': False}
+    init_goal = {'vpc': True, 'db': True, 'app': True}
+    ai_cloud_builder = Agent(name='CloudBuilder', state=init_state, goal=init_goal, actions=actions)
+    result = ai_cloud_builder.start()
+    pprint.pprint(result, indent=2, width=80)
 
-        [ ( 'CreateVPC',
-        <bound method Action.do of {'CreateVPC': {'pre_conditions': {'app': False, 'db': False, 'vpc': False}, 'effects': {'app': False, 'db': False, 'vpc': True}} }>),
-        ( 'CreateDB',
-        <bound method Action.do of {'CreateDB': {'pre_conditions': {'app': False, 'db': False, 'vpc': True}, 'effects': {'app': False, 'db': True, 'vpc': True}} }>),
-        ( 'CreateApp',
-        <bound method Action.do of {'CreateApp': {'pre_conditions': {'app': False, 'db': True, 'vpc': True}, 'effects': {'app': True, 'db': True, 'vpc': True}} }>)]
     """
 
     def __init__(self, name: str, state: dict, goal: dict, actions: object) -> object:
--- README.md
+++ README.md
@@ -14,4 +14,69 @@ To perform the search the planner sets a graph using the possible world states a
 
 ## Usage
 
+### Using the Agent class
+
+From the Agent.py class perspective the usage and interaction should be:
+
+```python
+from Goap.Action import Actions
+import pprint
+
+# ACTIONS
+actions = Actions()
+# VPC/Network set
+actions.add_action(
+    name='CreateVPC',
+    pre_conditions={'vpc': False, 'db': False, 'app': False},
+    effects={'vpc': True, 'db': False, 'app': False}
+)
+# DB set
+actions.add_action(
+    name='CreateDB',
+    pre_conditions={'vpc': True, 'db': False, 'app': False},
+    effects={'vpc': True, 'db': True, 'app': False}
+)
+actions.add_action(
+    name='StopDB',
+    pre_conditions={'vpc': True, 'db': 'started', 'app': False},
+    effects={'vpc': True, 'db': 'stopped', 'app': False}
+)
+actions.add_action(
+    name='StartDB',
+    pre_conditions={'vpc': True, 'db': 'stopped', 'app': False},
+    effects={'vpc': True, 'db': 'started', 'app': False}
+)
+actions.add_action(
+    name='DestroyDB',
+    pre_conditions={'vpc': True, 'db': 'not_health', 'app': False},
+    effects={'vpc': True, 'db': False, 'app': False}
+)
+# APP set
+actions.add_action(
+    name='CreateApp',
+    pre_conditions={'vpc': True, 'db': True, 'app': False},
+    effects={'vpc': True, 'db': True, 'app': True}
+)
+actions.add_action(
+    name='StartApp',
+    pre_conditions={'vpc': True, 'db': True, 'app': 'stopped'},
+    effects={'vpc': True, 'db': True, 'app': 'started'}
+)
+actions.add_action(
+    name='StopApp',
+    pre_conditions={'vpc': True, 'db': True, 'app': 'started'},
+    effects={'vpc': True, 'db': True, 'app': 'stopped'}
+)
+actions.add_action(
+    name='DestroyApp',
+    pre_conditions={'vpc': True, 'db': True, 'app': 'not_health'},
+    effects={'vpc': True, 'db': True, 'app': False}
+)
+init_state = {'vpc': False, 'app': False, 'db': False}
+init_goal = {'vpc': True, 'db': True, 'app': True}
+ai_cloud_builder = Agent(name='CloudBuilder', state=init_state, goal=init_goal, actions=actions)
+result = ai_cloud_builder.start()
+pprint.pprint(result, indent=2, width=80)
+```
+
 ## Version
