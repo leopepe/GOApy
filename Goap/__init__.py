fdb145161276f3d3e943561b80358948b462b8e8
Leonardo Pepe de Freitas <leopepe@me.com>
2017-08-05 11:54:39 -0300

    Automaton class commented.

--- Goap/__init__.py
+++ Goap/__init__.py
@@ -3,5 +3,13 @@ from Goap import Agent
 from Goap.Planner import Planner
 from Goap.StateMachine import StateMachine
 from Goap.Errors import OperationFailedError
+# from Goap.Automaton import Automaton
 
-__all__ = ['Agent', 'Action', 'Planner', 'StateMachine', 'Errors']
+__all__ = [
+    'Agent',
+    'Action',
+    'Planner',
+    'StateMachine',
+    'Errors',
+    # 'Automaton'
+]
