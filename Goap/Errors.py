3fcd61f23414c112484361b44d38d89556cbf05f
Leonardo Pepe de Freitas <leopepe@me.com>
2017-08-12 18:05:05 -0300

    Error class

--- Goap/Errors.py
+++ Goap/Errors.py
@@ -5,3 +5,23 @@ from __future__ import unicode_literals
 class OperationFailedError(Exception):
     def __init__(self, reason):
         self.msg = reason
+
+
+class SensorError(Exception):
+    """ Sensor's Error base class """
+    pass
+
+
+class SensorMultipleTypeError(SensorError):
+    """ Sensor can not be two type at once """
+    pass
+
+
+class SensorDoesNotExistError(SensorError):
+    """ Sensor do not exist """
+    pass
+
+
+class SensorAlreadyInCollectionError(SensorError):
+    """ Sensor do not exist """
+    pass
