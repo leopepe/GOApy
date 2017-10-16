502ab92309de7124c8022a030c98ae331624f976
Leonardo Pep <leonardo.pep@m4u.com.br>
2017-10-16 19:54:35 -0200

    Simpler structure taking advantage of automat FSM

--- Goap/Automaton.py
+++ Goap/Automaton.py
@@ -5,6 +5,14 @@ from Goap.Action import Actions
 from Goap.Planner import Planner
 from rx import Observable, Observer
 
+DEFAULT_WORLD_STATE = {
+    'vpc_state': 'Unknown',
+    'pub_subnet': 'Unknown',
+    'prv_subnet': 'Unknown',    # True || False
+    'db_state': 'Unknown',      # True || False
+    'app_state': 'Unknown',     # True || False
+}
+
 
 class Fact(object):
     def __init__(self, sensor, data, binding):
@@ -55,17 +63,6 @@ class WorldState(dict):
         del self.__dict__[key]
 
 
-WORLD_STATE = WorldState({
-    'vpc_state': 'Unknown',
-    'pub_subnet': 'Unknown',
-    'prv_subnet': 'Unknown',    # True || False
-    'db_state': 'Unknown',      # True || False
-    'instances_state': 'Unknown',     # True || False
-})
-
-WORKING_MEMORY = []
-
-
 class AutomatonPriorities:
 
     def __init__(self, items: list):
@@ -81,40 +78,24 @@ class AutomatonPriorities:
         return self.__repr__()
 
 
-class SensorResponseToFact(Observer):
-
-    def on_next(self, sensor):
-        WORKING_MEMORY.append(Fact(sensor=sensor.name, data=sensor.exec(), binding=sensor.binding))
-
-    def on_error(self, error):
-        return 'Error: {}'.format(error)
-
-    def on_completed(self):
-        return 'Done!'
-
-
-class AssimilateFact(Observer):
-
-    def on_next(self, fact):
-        setattr(WORLD_STATE, fact.binding, fact.data.output)
-
-    def on_error(self, error):
-        return 'Error: {}'.format(error)
-
-    def on_completed(self):
-        return 'Done!'
-
-
 class Automaton:
     """ A 3 State Machine Automaton: observing (aka monitor or patrol), planning and acting """
 
     machine = MethodicalMachine()
 
-    def __init__(self, name: str='Automaton', sensors: Sensors=[], actions: Actions=[]):
+    def __init__(self,
+                 name: str='Automaton',
+                 sensors: Sensors=[],
+                 actions: Actions=[],
+                 world_state: dict=DEFAULT_WORLD_STATE):
+        # setup
+        self.world_state = WorldState(world_state)
+        self.working_memory = []
         self.name = name
         self.sensors = sensors
         self.actions = actions
         self.planner = Planner(actions=actions)
+        #
         self.action_plan = []
         self.action_plan_response = None
         self.sensors_responses = {}
@@ -122,140 +103,134 @@ class Automaton:
         self.goal = {}
 
     def __sense_environment(self):
-        Observable.from_(self.sensors).subscribe(SensorResponseToFact())
-        Observable.from_(WORKING_MEMORY).subscribe(AssimilateFact())
+        Observable.from_(self.sensors). \
+            subscribe(
+            lambda sensor: self.working_memory.append(Fact(sensor=sensor.name, data=sensor.exec(), binding=sensor.binding))
+        )
+        Observable.from_(self.working_memory). \
+            subscribe(
+            lambda fact: setattr(self.world_state, fact.binding, fact.data.output)
+        )
 
-    @staticmethod
-    def get_world_state():
-        return WORLD_STATE
-
-    @machine.state(initial=True)
-    def idle(self):
-        """ Machine is ON and waiting for orders/goal """
-
-    @machine.input()
-    def input_goal(self, goal):
-        """ Input goal """
-
-    @machine.output()
-    def __set_goal(self, goal):
-        """ Set Automaton's goal """
-        self.goal = goal
-        try:
-            self.__sense_environment()
-        except IOError as err:
-            raise 'Error converting JSON data into Automatons knowledge {}'.format(err)
-
-    @machine.state()
-    def observing(self):
-        """ The automaton monitors/patrol the environment waiting for an event """
-
-    @machine.output()
-    def __report_all(self):
-        if WORLD_STATE != self.goal:
-            status = False
-        else:
-            status = True
-        return {
-            'env_state': WORLD_STATE,
-            'goal': self.goal,
-            'status': status,
-            'last_plan': self.action_plan,
-            'last_actions_results': self.action_plan_response
-        }
-
-    @machine.state()
-    def planning(self):
-        """ Planing a sequence of actions to change the environment """
-
-    @machine.input()
-    def plan(self):
-        """ Create a plan """
-
-    @machine.output()
-    def __formulate_action_plan(self):
-        self.action_plan = self.planner.plan(WORLD_STATE, self.goal)
+    def __set_action_plan(self):
+        self.action_plan = self.planner.plan(self.world_state, self.goal)
         return self.action_plan
 
-    @machine.state()
-    def acting(self):
-        """ Changing the environment """
-
-    @machine.input()
-    def execute_plan(self):
-        """ Change the environment """
-
-    @machine.input()
-    def goal_achieved(self):
-        """ Goal achieved """
-
-    @machine.output()
     def __execute_action_plan(self):
         self.actions_response = [action() for action in self.action_plan]
         return 'Action planning execution results: {}'.format(self.action_plan_response)
 
-    @machine.output()
-    def __get_action_plan_result(self):
-        return self.actions_response
+    @machine.state(initial=True)
+    def waiting_orders(self):
+        """ Waiting goal / orders """
+
+    @machine.state()
+    def sensing(self):
+        """ Running sensors and assimilating sensor's responses """
+
+    @machine.state()
+    def planning(self):
+        """ Generating action plan to change actual world state to achieve goal """
+
+    @machine.state()
+    def acting(self):
+        """ Executing action plan"""
 
     @machine.input()
-    def report(self):
-        """ Report actual knowledge and world state """
+    def sense(self):
+        """ Input sense state """
 
-    # idle -> observing [run sensors, return senors data]
-    idle.upon(input_goal, enter=observing, outputs=[__set_goal])
-    observing.upon(input_goal, enter=observing, outputs=[__set_goal])
-    observing.upon(report, enter=observing, outputs=[__report_all])
-    observing.upon(plan, enter=planning, outputs=[__formulate_action_plan])
-    planning.upon(execute_plan, enter=acting, outputs=[__execute_action_plan])
-    planning.upon(report, enter=planning, outputs=[__report_all])
-    acting.upon(goal_achieved, enter=idle, outputs=[__report_all])
-    acting.upon(report, enter=acting, outputs=[__report_all])
+    @machine.output()
+    def __sense(self):
+        """ Execute sensors """
+        self.__sense_environment()
+
+    @machine.input()
+    def plan(self):
+        """ Input for planning state """
+
+    @machine.output()
+    def __plan(self):
+        """ Generate action plan """
+        self.__set_action_plan()
+
+    @machine.input()
+    def act(self):
+        """ Input for acting state"""
+
+    @machine.output()
+    def __act(self):
+        """ Execute action plan """
+        self.__execute_action_plan()
+
+    @machine.input()
+    def input_goal(self, goal):
+        """ Change / Set AI goal """
+
+    @machine.output()
+    def __input_goal(self, goal):
+        """ Actually sets goal """
+        self.goal = goal
+
+    # cyclical main states
+    waiting_orders.upon(sense, enter=sensing, outputs=[__sense])
+    sensing.upon(plan, enter=planning, outputs=[__plan])
+    planning.upon(act, enter=acting, outputs=[__act])
+    acting.upon(sense, enter=sensing, outputs=[__sense])
+    # change orders
+    waiting_orders.upon(input_goal, enter=waiting_orders, outputs=[__input_goal])
+    planning.upon(input_goal, enter=waiting_orders, outputs=[__input_goal])
+    acting.upon(input_goal, enter=waiting_orders, outputs=[__input_goal])
 
 
 if __name__ == '__main__':
-    """ Proposed env_state:
+    """ proposed world_state:
     
-        {
-            "Vpc": {
-                "state": "available|doesnt_exist"
-            },
-            "Network": {
-                "private_subnet": false|true,
-                "public_subnet": false|true,
-                "internet_access": false|true,
-                "access_to_vpn": "yes|no",
-                "route_to_vpcs": ["vpn_2_office", "vpn_2_customer", ...]
-            },
-            "DB": {
-                "state": "available|doesnt_exist|"
-            }
-            "App": {
-                "type": "asg|instance",
-                "state": "available|doesnt_exist",
-                "node_count": 0
-            }
-        }
-
+        world_state_matrix = {
+            "vpc_state": 'Unknown',
+            "private_subnet": 'Unknown',
+            "public_subnet": 'Unknown',
+            "internet_access": 'Unknown',
+            "access_to_vpn": 'Unknown',
+            "db_state": 'Unknown',
+            "db_type": 'Unknown',
+            "app_state": 'Unknown',
+            "app_type": 'Unknown',
+            "node_count": 0
+    }
     
     """
+
     from pprint import PrettyPrinter
-    # from json import loads
-    # Instantiate
     pp = PrettyPrinter(indent=4)
     priorities = AutomatonPriorities([
-        {'vpc_state': 'available', 'db_state': 'available', 'instances_state': 'running'}
+        {'vpc_state': 'available', 'db_state': 'available', 'app_state': 'running'}
     ])
+    world_state_matrix = {
+        "vpc_state": 'Unknown',
+        "db_state": 'Unknown',
+        "app_state": 'Unknown',
+    }
+    goal = {
+        "vpc_state": "available",
+        "db_state": "available",
+        "app_state": "running",
+    }
     aws_actions = Actions()
-    aws_actions.add_action(
+    aws_actions.add(
         name='CreateVPC',
-        pre_conditions={'vpc_state': False, 'db_state': False, 'instances_state': False},
-        effects={'vpc_state': True, 'db_state': False, 'instances_state': False}
+        pre_conditions={'vpc_state': 'unavailable', 'db_state': 'unavailable', 'app_state': 'unavailable'},
+        effects={'vpc_state': 'available', 'db_state': 'unavailable', 'app_state': 'unavailable'}
     )
-    aws_actions.add_action(
+    aws_actions.add(
         name='CreateDB',
-        pre_conditions={'vpc_state': 'available', 'db_state': False, 'instances_state': False},
-        effects={'vpc_state': 'available', 'db_state': 'available', 'instances_state': False}
+        pre_conditions={'vpc_state': 'available', 'db_state': 'unavailable', 'app_state': 'unavailable'},
+        effects={'vpc_state': 'available', 'db_state': 'available', 'app_state': 'unavailable'}
+    )
+    aws_actions.add(
+        name='CreateApp',
+        pre_conditions={'vpc_state': 'available', 'db_state': 'available', 'app_state': 'unavailable'},
+        effects={'vpc_state': 'available', 'db_state': 'available', 'app_state': 'running'}
     )
     aws_sensors = Sensors()
     aws_sensors.add(
@@ -268,19 +243,26 @@ if __name__ == '__main__':
         shell='aws rds describe-db-instances --filters "Name=db-instance-id,Values=rds-oraculo" --query "DBInstances[].DBInstanceStatus" --output text',
         binding='db_state'
     )
+    # aws_sensors.add(
+    #     name='FindProjectInstances',
+    #     shell='aws ec2 describe-instances --filters "Name=tag-key,Values=project","Name=tag-value,Values=mesos-master" --query "Reservations[].Instances[].State" --output text|awk \'{print $2}\'',
+    #     binding='app_state'
+    # )
     aws_sensors.add(
-        name='FindProjectInstances',
-        shell='aws ec2 describe-instances --filters "Name=tag-key,Values=project","Name=tag-value,Values=mesos-master" --query "Reservations[].Instances[].State" --output text|awk \'{print $2}\'',
-        binding='instances_state'
+        name='CheckAppState',
+        shell='echo "unavailable"',
+        binding='app_state'
     )
-    ai = Automaton(name='infra_builder', actions=aws_actions, sensors=aws_sensors)
+    ai = Automaton(name='infra_builder', actions=aws_actions, sensors=aws_sensors, world_state=world_state_matrix)
     # Control
     # what is the environment status? what does the sensors return? ai has a goal?
     # goal = priorities # object not working returning object rather then dict
-    goal = {'vpc_state': 'available', 'db_state': 'available', 'instances_state': 'running'}
-    recon_world = ai.input_goal(goal)
-    action_plan = ai.plan()
-    result = ai.execute_plan()
-    pp.pprint('Acknowledge world: {}, Action Plan: {}, Result: {}'.format(recon_world, action_plan, result))
-    pp.pprint(ai.report())
+    ai.input_goal(goal)
+    print(ai.world_state)
+    ai.sense()
+    print(ai.world_state)
+    ai.plan()
+    pp.pprint(
+        'Acknowledge world: {}, Action Plan: {}, Result: {}'.format(ai.world_state, ai.action_plan, ai.actions_response)
+    )
 
