9af5c248d955052fd094b7a19225b5f2c089a6ba
leopepe <lpepefreitas@gmail.com>
2018-11-07 22:36:46 +0100

    Refactor on Action class and models. Action classes are compatible with Sensors class.

--- Goap/Action.py
+++ Goap/Action.py
@@ -2,135 +2,7 @@ import subprocess
 from filecmp import cmp
 from json import dumps
 
-
-class ActionResponse:
-    def __init__(self, **kwargs):
-        self.sensor_type = kwargs.get('type', None)
-        self.sensor_response = self.__adapt_response(**kwargs)
-
-    def __adapt_response(self, **kwargs):
-        if self.sensor_type == 'shell':
-            response = ShellCommandResponse(**kwargs)
-        elif self.sensor_type == 'awsapi':
-            response = AWSCommandResponse(**kwargs)
-        else:
-            raise '{} Need to specify the sensor\'s type'.format(ActionError)
-        return response
-
-
-class ShellCommandResponse:
-    """ ShellCommand Return is a class for represent the agent's actions return """
-
-    def __init__(self, **kwargs):
-        """
-
-        :rtype: object
-        :param kwargs: return_code, output and error
-        """
-        self.name = kwargs.get('name', None)
-        self.return_code = kwargs.get('return_code', None)
-        self.output = str(kwargs.get('output', None)).replace('\n', '').replace('\t', '')
-        self.error = kwargs.get('error', None).replace('\n', '').replace('\t', '')
-
-    def __return_message(self):
-        if self.return_code == 0 and self.__check_response():
-            return {'return_code': self.return_code, 'output': str(self.output).replace('\n', '').replace('\t', '')}
-        elif self.return_code == 0 and not self.__check_response():
-            return {'return_code': self.return_code, 'error': str('False')}
-        else:
-            return {'return_code': self.return_code, 'error': str(self.error).replace('\n', '').replace('\t', '')}
-
-    def __check_response(self):
-        if self.output == '' or self.output == '\n':
-            return False
-        else:
-            return True
-
-    def __repr__(self):
-        return str(self.__return_message())
-
-    def __str__(self):
-        return self.__repr__()
-
-
-class ShellCommand:
-    """ Shell Sensor object factory
-
-    """
-
-    def __init__(self, name: str, pre_conditions: dict, effects: dict, shell: str):
-        """ *nix shell Command
-
-        """
-        self.name = name
-        self.pre_conditions = pre_conditions
-        self.effects = effects
-        self.type = 'shell'
-        self.shell = shell
-        self.return_code = None
-        self.stdout = None
-        self.stderr = None
-        self.response = {}
-
-    def __repr__(self):
-        return 'Name: {}'.format(self.name)
-
-    def __str__(self):
-        return self.__repr__()
-
-    def exec(self):
-        """ Executes the sensor. Sensor's can be objects or a cli command
-
-        :return SensorResponse:
-        """
-        cmd = self.shell
-        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        try:
-            stdout, stderr = proc.communicate(timeout=30)
-            returncode = proc.returncode
-            self.response = ShellCommandResponse(name=self.name, output=stdout, error=stderr, return_code=returncode)
-        except TimeoutError as e:
-            proc.kill()
-            raise('{}'.format(e))
-
-        return self.response
-
-
-class AWSCommandResponse:
-    """ CommandReturn is a class for represent the agent's sensor's return """
-
-    def __init__(self, **kwargs):
-        """
-
-        :param kwargs: expects response as dict argument and parse aws http response
-        """
-        self.return_code = None
-        self.output = None
-        self.error = None
-        self.name = kwargs.get('name', None)
-        self.sensor_response = kwargs.get('response', None)
-        self._json_parsed_response = None
-
-        self.__parse_aws_api_http_response()
-
-    def __parse_aws_api_http_response(self):
-        self._json_parsed_response = json.loads(self.sensor_response)
-        self.return_code = self._json_parsed_response['ResponseMetadata']['HTTPStatusCode']
-        if self.return_code == '200':
-            self.output = self._json_parsed_response
-        else:
-            self.error = self._json_parsed_response
-
-    def __repr__(self):
-        json_data = {}
-        if self.output:
-            json_data = {'return_code': self.return_code, 'output': str(self.output)}
-        elif self.error:
-            json_data = {'return_code': self.return_code, 'error': str(self.error)}
-        return json.dumps(json_data, skipkeys=True)
-
-    def __str__(self):
-        return self.__repr__()
+from Goap.Errors import *
 
 
 class Action:
@@ -153,12 +25,85 @@ class Action:
     def __hash__(self):
         return hash(self)
 
-    def __call__(self):
+    def __call__(self, **kwargs):
+        self.__init__(kwargs.get('name'), kwargs.get('pre_conditions'), kwargs.get('effects'))
         self.exec()
 
     def exec(self) -> tuple:
-        # print(self.name)
-        return self.name, True
+        pass
+
+
+class ActionResponse:
+
+    def __init__(self, name: str, action_type: str, return_code: str, stdout: str='', stderr: str=''):
+        """
+        
+        :param name: 
+        :param action_type: 
+        :param return_code: 
+        :param stdout: 
+        :param stderr: 
+        """
+        self.name = name
+        self.action_type = action_type
+        self.return_code = return_code
+        self.stdout = stdout
+        self.stderr = stderr
+        self.__response_parser()
+
+    def __str__(self):
+        return 'Name: {}, Response: {}, ReturnCode: {}'.format(self.name, self.response, self.return_code)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __response_parser(self):
+        if not self.stdout == '':
+            self.response = self.stdout
+        elif not self.stderr == '':
+            self.response = self.stderr
+
+
+class ShellAction(Action):
+
+    def __init__(self, name: str, pre_conditions: dict, effects: dict, shell: str):
+        self.response = {}
+        self.type = 'shell'
+        self.shell = shell
+        Action.__init__(self, name=name, pre_conditions=pre_conditions, effects=effects)
+
+    def __repr__(self):
+        return 'Name: {}'.format(self.name)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def exec(self):
+        cmd = self.shell
+        process = subprocess.Popen(
+            cmd,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True
+        )
+        try:
+            stdout, stderr = process.communicate(timeout=30)
+            return_code = process.returncode
+            self.response = actionResponse(
+                name=self.name,
+                action_type='shell',
+                stdout=stdout,
+                stderr=stderr,
+                return_code=return_code
+            )
+        except TimeoutError as e:
+            process.kill()
+            raise('{}'.format(e))
+        finally:
+            process.kill()
+
+        return self.response
 
 
 class Actions:
@@ -185,34 +130,6 @@ class Actions:
             else:
                 return None
 
-    def add(self, **kwargs):
-        """
-
-        :param kwargs:
-        :return:
-        """
-        if kwargs.get('shell') and kwargs.get('obj'):
-            raise ActionMultipleTypeError
-
-        name = kwargs.get('name')
-        pre_conditions = kwargs.get('pre_conditions')
-        effects = kwargs.get('effects')
-        shell = kwargs.get('shell', None)
-        obj = kwargs.get('obj', None)
-        binding = kwargs.get('binding', None)
-        if shell:
-            self.add_shell_command(name, pre_conditions, effects, shell)
-        elif obj:
-            self.add_obj_command(name, obj, binding)
-
-    def add_shell_command(self, name, pre_conditions, effects, shell):
-        # add action to self.actions
-        self.actions.append(ShellCommand(name, pre_conditions, effects, shell))
-
-    def remove(self, name: str):
-        # traverse self.actions and remove named action
-        [self.actions.remove(action) for action in self.actions if action.name == name]
-
     def get(self, name):
         result = None
         for action in self.actions:
@@ -233,6 +150,41 @@ class Actions:
                 state_grid.append(a.effects)
         return state_grid
 
+    def __add_shell_action(self, name, shell, pre_conditions, effects):
+        if not ShellAction(name=name, shell=shell, pre_conditions=pre_conditions, effects=effects) in self.actions:
+            self.actions.append(ShellAction(name=name, shell=shell, pre_conditions=pre_conditions, effects=effects))
+        else:
+            raise actionAlreadyInCollectionError
+
+    def __add_obj_action(self, name, obj, pre_conditions, effects):
+        raise NotImplementedError
+
+    def add(self, **kwargs):
+        if kwargs.get('shell') and kwargs.get('obj'):
+            raise ActionMultipleTypeError
+
+        name = kwargs.get('name')
+        shell = kwargs.get('shell', None)
+        obj = kwargs.get('obj', None)
+        pre_conditions = kwargs.get('pre_conditions', None)
+        effects = kwargs.get('effects', None)
+        if shell:
+            self.__add_shell_action(name, shell, pre_conditions, effects)
+        elif obj:
+            self.__add_obj_action(name, obj, pre_conditions, effects)
+
+    def remove(self, name: str):
+        result = False
+        for action in self.actions:
+            if action.name == name:
+                self.actions.remove(action)
+                result = True
+        return result
+
+    def exec_all(self) -> list:
+        responses = [s.exec() for s in self.actions]
+        return responses
+
     @staticmethod
     def compare_actions(action1: Action, action2: Action):
         result = None
@@ -244,58 +196,76 @@ class Actions:
 
 if __name__ == '__main__':
     # ACTIONS
-    actions = Actions()
+    aws_actions = Actions()
     # VPC/Network set
-    actions.add(
+    aws_actions.add(
         name='CreateVPC',
         pre_conditions={'vpc': False, 'db': False, 'app': False},
-        effects={'vpc': True, 'db': False, 'app': False}
+        effects={'vpc': True, 'db': False, 'app': False},
+        shell='awscli --help'
     )
     # DB set
-    actions.add(
+    aws_actions.add(
         name='CreateDB',
         pre_conditions={'vpc': True, 'db': False, 'app': False},
-        effects={'vpc': True, 'db': True, 'app': False}
+        effects={'vpc': True, 'db': True, 'app': False},
+        shell='awscli --help'
     )
-    actions.add(
+    aws_actions.add(
         name='StopDB',
         pre_conditions={'vpc': True, 'db': 'started', 'app': False},
-        effects={'vpc': True, 'db': 'stopped', 'app': False}
+        effects={'vpc': True, 'db': 'stopped', 'app': False},
+        shell='awscli --help'
     )
-    actions.add(
+    aws_actions.add(
         name='StartDB',
         pre_conditions={'vpc': True, 'db': 'stopped', 'app': False},
-        effects={'vpc': True, 'db': 'started', 'app': False}
+        effects={'vpc': True, 'db': 'started', 'app': False},
+        shell='awscli --help'
     )
-    actions.add(
+    aws_actions.add(
         name='DestroyDB',
         pre_conditions={'vpc': True, 'db': 'not_health', 'app': False},
-        effects={'vpc': True, 'db': False, 'app': False}
+        effects={'vpc': True, 'db': False, 'app': False},
+        shell='awscli --help'
     )
     # APP set
-    actions.add(
+    aws_actions.add(
         name='CreateApp',
         pre_conditions={'vpc': True, 'db': True, 'app': False},
-        effects={'vpc': True, 'db': True, 'app': True}
+        effects={'vpc': True, 'db': True, 'app': True},
+        shell='awscli --help'
     )
-    actions.add(
+    aws_actions.add(
         name='StartApp',
         pre_conditions={'vpc': True, 'db': True, 'app': 'stopped'},
-        effects={'vpc': True, 'db': True, 'app': 'started'}
+        effects={'vpc': True, 'db': True, 'app': 'started'},
+        shell='awscli --help'
     )
-    actions.add(
+    aws_actions.add(
         name='StopApp',
         pre_conditions={'vpc': True, 'db': True, 'app': 'started'},
-        effects={'vpc': True, 'db': True, 'app': 'stopped'}
+        effects={'vpc': True, 'db': True, 'app': 'stopped'},
+        shell='awscli --help'
     )
-    actions.add(
+    aws_actions.add(
         name='DestroyApp',
         pre_conditions={'vpc': True, 'db': True, 'app': 'not_health'},
-        effects={'vpc': True, 'db': True, 'app': False}
+        effects={'vpc': True, 'db': True, 'app': False},
+        shell='awscli --help'
     )
-    print('{0}\n{1}'.format(actions, actions.__len__()))
-    actions.remove(name='CreateVPC')
-    print('{0}\n{1}'.format(actions, actions.__len__()))
-    print(actions.get(name='CreateDB'))
+    print('{0}\n{1}'.format(aws_actions, aws_actions.__len__()))
+    aws_actions.remove(name='CreateVPC')
+    print('{0}\n{1}'.format(aws_actions, aws_actions.__len__()))
+    print(aws_actions.get(name='CreateDB'))
     # action.do() returns a tuple
-    print(type(actions.get(name='CreateDB').do()), actions.get(name='CreateDB').do())
+    print(type(aws_actions.get(name='CreateDB')), aws_actions.get(name='CreateDB'))
+    fs_actions = Actions()
+    fs_actions.add(
+        name='CompactBigLogFiles',
+        pre_conditions={'files_to_compact': 'Exists'},
+        effects={'files_to_compact': 'None'},
+        shell='find /tmp -name "*.log" -type f -size +900M| xargs tar -zcvf logfile-$(date "+%d%m%y-%H%M%S").tar.gz {}'
+    )
+    print(fs_actions)
+
