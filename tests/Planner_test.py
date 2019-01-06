21d9c300a115c8bafadb9a802c9053dde59a268f
leopepe <lpepefreitas@gmail.com>
2019-01-06 20:07:24 +0100

    Unnit tests for planner

---
+++ tests/Planner_test.py
@@ -0,0 +1,71 @@
+from os import path
+import unittest
+import subprocess
+from pprint import PrettyPrinter
+from Goap.Action import Actions
+from Goap.Sensor import Sensors
+from Goap.Automaton import Planner
+
+
+class PlannerTest(unittest.TestCase):
+
+    @staticmethod
+    def __reset_environment():
+        if path.isdir('/tmp/goap_tmp'):
+            subprocess.call(['rm', '-rf', '/tmp/goap_tmp'])
+
+    def __print(self):
+        self.print.pprint(
+            'Nodes: {}, Edges: {}'.format(self.planner.nodes, self.planner.edges)
+        )
+
+    def setUp(self):
+        self.print = PrettyPrinter(indent=4)
+        self.world_state_matrix = {
+            "tmp_dir_state": 'not_exist',
+            "tmp_dir_content": 'token_not_found',
+        }
+        self.goal = {
+            "tmp_dir_state": "exist",
+            "tmp_dir_content": "token_found",
+        }
+        self.actions = Actions()
+        self.actions.add(
+            name='CreateTmpDir',
+            pre_conditions={'tmp_dir_state': 'not_exist', 'tmp_dir_content': 'token_not_found'},
+            effects={'tmp_dir_state': 'exist', 'tmp_dir_content': 'token_not_found'},
+            shell='mkdir -p /tmp/goap_tmp'
+        )
+        self.actions.add(
+            name='CreateToken',
+            pre_conditions={'tmp_dir_state': 'exist', 'tmp_dir_content': 'token_not_found'},
+            effects={'tmp_dir_state': 'exist', 'tmp_dir_content': 'token_found'},
+            shell='touch /tmp/goap_tmp/.token'
+        )
+        self.planner = Planner(actions=self.actions)
+
+    def test_set_goal(self):
+        self.planner.goal = self.goal
+        assert self.goal == self.planner.goal
+
+    def test_nodes(self):
+        assert self.planner.nodes == [
+            (0, {'tmp_dir_state': 'not_exist', 'tmp_dir_content': 'token_not_found'}),
+            (1, {'tmp_dir_state': 'exist', 'tmp_dir_content': 'token_not_found'}),
+            (2, {'tmp_dir_state': 'exist', 'tmp_dir_content': 'token_found'})
+        ]
+
+    def test_edges(self):
+        assert self.planner.edges == [
+            (0, 1, {'object': self.actions.get('CreateTmpDir')}),
+            (1, 2, {'object': self.actions.get('CreateToken')})
+        ]
+
+    def test_planning(self):
+        self.planner.plan(init_state=self.world_state_matrix, goal=self.goal)
+        assert self.planner.action_plan == [
+            (0, 1, {'object': self.actions.get('CreateTmpDir')}),
+            (1, 2, {'object': self.actions.get('CreateToken')})
+        ]
+
+
