e4c964318a7484dd93d3b5ec5bca1b58acfdc0c7
leopepe <lpepefreitas@gmail.com>
2019-01-06 14:44:19 +0100

    fixing assert on object name signature

--- tests/Action_test.py
+++ tests/Action_test.py
@@ -38,7 +38,7 @@ class ActionTest(unittest.TestCase):
             shell='awscli vpc create'
         )
         self.actions.remove(name='CreateVPC')
-        assert 'Name: CreateDB' == str(self.actions.get(name='CreateDB'))
+        assert 'CreateDB' == str(self.actions.get(name='CreateDB'))
 
     def test_remove_action_error(self):
         self.actions.add(
