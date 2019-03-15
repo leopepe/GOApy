d28e51b88ba4bbc1737c5520e4683aabd11a6a7a
leopepe <lpepefreitas@gmail.com>
2019-03-15 14:20:40 +0100

    plan and path seems to be ok, need to update the unit tests

--- Goap/Planner2.py
+++ Goap/Planner2.py
@@ -19,17 +19,52 @@ class Node(object):
 
 class Edge(object):
 
-    def __init__(self, name, predecessor: Node, successor: Node, cost: float = 0.0):
+    def __init__(self, name, predecessor: Node, successor: Node, cost: float = 0.0, obj: object = None):
         self.name = name
         self.cost = cost
         self.predecessor = predecessor
         self.successor = successor
+        self.obj = obj
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
+
+
+class Nodes(object):
+
+    def __init__(self):
+        self.nodes = []
+
+    def __add__(self, other: Node):
+        self.nodes.append(other)
+
+    def __iter__(self):
+        return self.nodes
+
+    def add(self, other: Node):
+        self.__add__(other)
+
+
+class Edges(object):
+
+    def __init__(self, edges: list = None):
+        if edges:
+            for edge in edges:
+                self.add(edge)
+        else:
+            self.edges = []
+
+    def __add__(self, other: Edge):
+        self.edges.append(other)
+
+    def __iter__(self):
+        return self.edges
+
+    def add(self, other: Edge):
+        self.__add__(other)
 
 
 class Graph(object):
@@ -57,7 +92,7 @@ class Graph(object):
         self.directed.add_node(node, attr_dict=attribute, label=node.name, object=node)
 
     def __add_edge(self, edge: Edge):
-        self.directed.add_edge(edge.predecessor, edge.successor, object=edge, weight=edge.cost, label=edge.name)
+        self.directed.add_edge(edge.predecessor, edge.successor, object=edge.obj, weight=edge.cost, label=edge.name)
 
     def add_nodes_from(self, nodes: list):
         [self.__add_node(node, attribute=node.attributes) for node in nodes]
@@ -100,11 +135,13 @@ class Graph(object):
                 node_size=1200,
                 node_color='lightblue',
                 linewidths=0.25,
-                font_size=10,
+                font_size=8,
                 font_weight='bold',
                 with_labels=True,
-                dpi=1000
+                dpi=5000
             )
+            # edge_labels = nx.get_edge_attributes(self.directed, name='attr_dict')
+            # nx.draw_networkx_edge_labels(self.directed, pos=pos, edge_labels=edge_labels)
             plt.savefig(file_path)
         except IOError as err:
             raise('Could not create plot image: {}', err)
@@ -127,7 +164,7 @@ class Planner(object):
     @staticmethod
     def __isinlist(dic: dict, l: list):
         for d in l:
-            if dic == d:
+            if dic == d.attributes:
                 return True
         return False
 
@@ -137,8 +174,8 @@ class Planner(object):
 
     def __generate_states(self, actions, world_state):
         states = []
-        if not states:
-            states.append(Node(attributes=world_state))
+        # if not states:
+        #     states.append(Node(attributes=world_state))
 
         for action in actions:
             pre = {**world_state, **action.pre_conditions}
@@ -149,18 +186,33 @@ class Planner(object):
                 states.append(Node(attributes=eff))
         return states
 
+    # @staticmethod
+    # def __generate_transitions(actions, states):
+    #     edges = []
+    #     pre, suc = None, None
+    #     for state in states:
+    #         for action in actions:
+    #             if action.pre_conditions.items() <= state.attributes.items():
+    #                 pre = state
+    #             if action.effects.items() <= state.attributes.items():
+    #                 suc = state
+    #             if pre and suc:
+    #                 edges.append(Edge(name=action.name, predecessor=pre, successor=suc, cost=action.cost, obj=action))
+    #                 pre, suc = None, None
+    #     return edges
+
     @staticmethod
     def __generate_transitions(actions, states):
         edges = []
         pre, suc = None, None
-        for state in states:
-            for action in actions:
+        for action in actions:
+            for state in states:
                 if action.pre_conditions.items() <= state.attributes.items():
                     pre = state
                 if action.effects.items() <= state.attributes.items():
                     suc = state
                 if pre and suc:
-                    edges.append(Edge(name=action.name, predecessor=pre, successor=suc, cost=action.cost))
+                    edges.append(Edge(name=action.name, predecessor=pre, successor=suc, cost=action.cost, obj=action))
                     pre, suc = None, None
         return edges
 
@@ -184,52 +236,98 @@ if __name__ == '__main__':
     init_ws = WorldState(lv_need_expansion=False, vg_need_expansion=False)
     ws = WorldState(lv_need_expansion=True, vg_need_expansion=True)
     gs = WorldState(lv_need_expansion=False, vg_need_expansion=False)
-    #
-    acts = Actions()
-    acts.add(
-        name='ExpandLV',
-        pre_conditions={
-            'lv_need_expansion': True,
-            'vg_need_expansion': False,
-        },
-        effects={
-            'lv_need_expansion': False,
-        },
-        shell='echo expand_lv',
-        cost=1.0
-    )
-    acts.add(
-        name='ExpandVG',
-        pre_conditions={
-            'vg_need_expansion': True,
-        },
-        effects={
-            'vg_need_expansion': False,
-        },
-        shell='echo expand_vg'
-    )
-    acts.add(
-        name='PurgeOldFiles',
-        pre_conditions={
-            'lv_need_expansion': True,
-            'vg_need_expansion': True,
-        },
-        effects={
-            'lv_need_expansion': False,
-        },
-        shell='echo purge_old_files',
-        cost=1.5,
-    )
-    p = Planner(world_state=init_ws, actions=acts)
-    p.graph.plot('graph.png')
-    print(p.graph.nodes(data=True))
-    print(p.graph.size)
-    print(p.graph.edges(data=True))
-    # for n in p.graph.nodes(data=True):
-    #     print(n)
-    # print(world_state)
-    # print(p.graph.edge_between_nodes(src='{lv_state: out_of_capacity, vg_state: ok}', dst='{lv_state: ok}'))
-    print(p.graph.path(ws, gs))
-    print(p.plan(ws, gs))
-    # print(gs)
-    # print(p.graph.get_node(gs))
+
+    def setupPlanner():
+        acts = Actions()
+        acts.add(
+            name='ExpandLV',
+            pre_conditions={
+                'lv_need_expansion': True,
+                'vg_need_expansion': False,
+            },
+            effects={
+                'lv_need_expansion': False,
+            },
+            shell='echo expand_lv',
+            cost=1.0
+        )
+        acts.add(
+            name='ExpandVG',
+            pre_conditions={
+                'vg_need_expansion': True,
+            },
+            effects={
+                'vg_need_expansion': False,
+            },
+            shell='echo expand_vg'
+        )
+        acts.add(
+            name='PurgeOldFiles',
+            pre_conditions={
+                'lv_need_expansion': True,
+                'vg_need_expansion': True,
+            },
+            effects={
+                'lv_need_expansion': False,
+            },
+            shell='echo purge_old_files',
+            cost=1.5,
+        )
+        return Planner(world_state=init_ws, actions=acts)
+
+    p = setupPlanner()
+
+    def printNodes(data: bool = True):
+        print(p.graph.nodes(data=data))
+
+    def printEdges(data: bool = True):
+        print(p.graph.edges(data=data))
+
+    def printSize():
+        print(p.graph.size)
+
+    def plotGraph():
+        p.graph.plot('graph.png')
+
+    def printWS():
+        print(ws)
+
+    def printPath():
+        print(p.graph.path(ws, gs))
+
+    def printPlan():
+        print(p.plan(ws, gs))
+
+    plotGraph()
+
+    printPath()
+
+    printPlan()
+
+    # print(p.transitions)
+    # for t in p.transitions:
+    #     print(t.obj)
+
+    # print(p.plan(ws, gs))
+    # # print(gs)
+    # # print(p.graph.get_node(gs))
+    # dir_acts = Actions()
+    # dir_acts.add(
+    #     name='CreateTmpDir',
+    #     pre_conditions={'tmp_dir_state': False, 'tmp_dir_content': False},
+    #     effects={'tmp_dir_state': True, 'tmp_dir_content': False},
+    #     shell='mkdir -p /tmp/goap_tmp'
+    # )
+    # dir_acts.add(
+    #     name='CreateToken',
+    #     pre_conditions={'tmp_dir_state': True, 'tmp_dir_content': False},
+    #     effects={'tmp_dir_state': True, 'tmp_dir_content': True},
+    #     shell='touch /tmp/goap_tmp/.token'
+    # )
+    # dir_init_ws = WorldState({"tmp_dir_state": False, "tmp_dir_content": False, })
+    # dir_gs = WorldState({"tmp_dir_state": True, "tmp_dir_content": True, })
+    # dir_handler = Planner(world_state=dir_init_ws, actions=dir_acts)
+    # print(dir_handler.graph.nodes(data=True))
+    # print(dir_handler.graph.size)
+    # print(dir_handler.graph.edges(data=True))
+    # print(dir_handler.graph.path(dir_init_ws, dir_gs))
