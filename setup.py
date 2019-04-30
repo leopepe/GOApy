34efb198dba832e86ce942b3d6a69fecd78ff36e
leopepe <lpepefreitas@gmail.com>
2019-04-30 11:56:26 +0200

    adding project URL

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
     name=Goap.__name__,
     version=Goap.__version__,
     packages=find_packages(),
-    url='',
+    url='https://github.com/leopepe/GOApy',
     license='Simplified BSD License',
     author='Leonardo Pêpe',
     author_email='lpepefreitas@gmail.com',
-    description='Goal-Oriented Action Planning implementation in Python. Inspired by Jeff Orkin <>',
+    description='Goal-Oriented Action Planning implementation in Python. Inspired by Jeff Orkin GOAP <>',
     test_suite='tests',
 )
